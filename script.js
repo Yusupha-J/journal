db070b2417f1fc4022cb09ec24e301e6ee7d971d
Yusupha-J <yusupha.jawara@latoile.em-lyon.com>
2025-05-21 17:40:41 +0200

    update+

--- script.js
+++ script.js
@@ -11,9 +11,16 @@ function getData() {
        console.log('Données récupérées du fichier JSON :', data);
        /// ON ECRIT LE CODE ICI ! 
       
-       let animauxContainer = document.getElementById('animaux-container');
-      console.log(animauxContainer);
+       let articlePricipal = document.getElementById('article-prncipal');
+      console.log(articlePricipal);
       
+      let dataArticlePrincipal = data.journal.articlePrincipal;
+
+      let h1 = document.createElement('h1');               
+      h1.textContent = dataArticlePrincipal.titre
+
+      articlePricipal.appendChild(h1)
+          
 
        /// FIN DU CODE
      })
